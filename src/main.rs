c235a5fff5787c7ea169c8401f88894c27970bd1
Constantin Goeldel <constantin.goeldel@tum.de>
2022-11-27 16:53:29 +0000

    fix window

--- src/main.rs
+++ src/main.rs
@@ -386,35 +386,36 @@ fn set_up_output_dir(
 10_8 11_8",
     );
     for i in 0..window_count {
+        let window = i * window_size;
         let nodelist = format!(
             "filename,node,gen,meth
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G0_All.txt,0_0,0,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G1_L2_All.txt,1_2,1,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G1_L8_All.txt,1_8,1,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G2_L2_All.txt,2_2,2,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G2_L8_All.txt,2_8,2,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G0_All.txt,0_0,0,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G1_L2_All.txt,1_2,1,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G1_L8_All.txt,1_8,1,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G2_L2_All.txt,2_2,2,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G2_L8_All.txt,2_8,2,Y
 -,3_2,3,N
 -,3_8,3,N
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G4_L2_All.txt,4_2,4,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G4_L8_All.txt,4_8,4,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G5_L2_All.txt,5_2,5,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G5_L8_All.txt,5_8,5,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G4_L2_All.txt,4_2,4,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G4_L8_All.txt,4_8,4,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G5_L2_All.txt,5_2,5,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G5_L8_All.txt,5_8,5,Y
 -,6_2,6,N
 -,6_8,6,N
 -,7_2,7,N
 -,7_8,7,N
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G8_L2_All.txt,8_2,8,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G8_L8_All.txt,8_8,8,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G8_L2_All.txt,8_2,8,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G8_L8_All.txt,8_8,8,Y
 -,9_2,9,N
 -,9_8,9,N
 -,10_2,10,N
 -,10_8,10,N
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G11_L2_All.txt,11_2,11,Y
-/mnt/extStorage/constantin/windows/{i}/methylome_Col0_G11_L8_All.txt,11_8,11,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G11_L2_All.txt,11_2,11,Y
+/mnt/extStorage/constantin/windows/{window}/methylome_Col0_G11_L8_All.txt,11_8,11,Y
 "
         );
 
-        let path = format!("{}/{}", &output_path, i * window_size);
+        let path = format!("{}/{}", &output_path, window);
         let window_dir = fs::read_dir(&path);
 
         match window_dir {
